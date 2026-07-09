ee9af84c9b6f828ec1b48e7c0e191ef010db34bd
Daniel Fimiani <danielfimiani@gmail.com>
2026-07-09 18:59:16 -0300

    feat: building center & gym

---
+++ features/overworld/buildings/building.gd
@@ -0,0 +1,14 @@
+@tool
+extends StaticBody3D
+
+@onready var door_trigger: Area3D = $DoorTigger
+
+@export var interior: PackedScene
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		door_trigger.area_entered.connect(_on_door_entered)
+
+func _on_door_entered(_area: Area3D) -> void:
+	if interior:
+		get_tree().change_scene_to_packed(interior)
