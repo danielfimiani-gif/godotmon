50dc5acd017a605646aa9dac9e81e73e23ba185f
Daniel Fimiani <danielfimiani@gmail.com>
2026-07-09 14:43:07 -0300

    feat: grass detection logic

--- features/overworld/player.gd
+++ features/overworld/player.gd
@@ -1,9 +1,17 @@
 extends Node3D
 
+@onready var grass_detector: Area3D = $GrassDetector
+
 const TILE := 1.0
 const MOVE_TIME := 0.15
+const ENCOUNTER_CHANCE := 0.2
 
 var moving := false
+var in_grass := false
+
+func _ready() -> void:
+	grass_detector.area_entered.connect(_on_grass_entered)
+	grass_detector.area_exited.connect(_on_grass_exited)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if moving:
@@ -23,3 +31,16 @@ func _step(dir: Vector3) -> void:
 	tween.tween_property(self, "position", target, MOVE_TIME)
 	await tween.finished
 	moving = false
+	_check_encounter()
+
+func _on_grass_entered(area: Area3D) -> void:
+	if area.is_in_group("tall_grass"):
+		in_grass = true
+
+func _on_grass_exited(area: Area3D) -> void:
+	if area.is_in_group("tall_grass"):
+		in_grass = false
+
+func _check_encounter() -> void:
+	if in_grass and randf() < ENCOUNTER_CHANCE:
+		print("!Un Mon salvaje apareció!")
