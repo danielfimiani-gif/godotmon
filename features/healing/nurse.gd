5d7669001aaa69d5480c576577c7ba6673997ae4
Daniel Fimiani <danielfimiani@gmail.com>
2026-07-10 12:08:27 -0300

    feat: refactor nurse

--- features/healing/nurse.gd
+++ features/healing/nurse.gd
@@ -3,7 +3,7 @@ extends Area3D
 func _ready() -> void:
 	area_entered.connect(_on_entered)
 
-func _on_entered(area: Area3D) -> void:
+func _on_entered(_area: Area3D) -> void:
 	GameState.heal_all()
 	print("!Tu equipo está como nuevo")
 
